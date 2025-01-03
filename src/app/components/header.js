97d508c087be732c3864c34f5a543f478b2fb138
implaymo <goncalomoreirasilva@gmail.com>
2025-01-03 10:09:26 +0000

    Header is working

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <Head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Pokedex</title>
+      <title>FDSAHGJDASHK</title>
       {/* Favicon */}
       <link rel="icon" href="/images/pokeball.png" type="image/png" />
       {/* Bootstrap CSS */}
